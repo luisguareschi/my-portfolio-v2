81e9f3b8ac8fc93c43f4166b535de06bf7384147
luisguareschi <luisguareschi@hotmail.com>
2023-05-16 00:43:46 +0200

    Bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import styles from './page.module.css'
 import NavBar from "@/components/NavBar/NavBar";
 import Homepage from "@/app/sections/homepage/Homepage";
 import Sidebars from "@/components/Sidebars/Sidebars";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import LoadingScreen from "@/app/sections/loadingScreen/LoadingScreen";
 
 export default function Home() {
     const [loading, setLoading] = useState(true)
     if (loading) {
         return (
-            <LoadingScreen setLoading={(val)=>setLoading(val)}/>
+            <LoadingScreen setLoading={(val: any)=>setLoading(val)}/>
         )
     }
     return (
